3f7138b82a4bbbad9fffa258f5c0c99824a70310
anandsoni414 <anand414soni@gmail.com>
2026-06-21 18:26:15 +0530

    Hero component is made

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import {Route,Routes} from 'react-router-dom'
 import Home from './pages/Home';
 import ApplyJob from './pages/ApplyJob';
 import Application from './pages/Application';
+
 function App() {
   return (
     <div>
        <Routes>
            <Route path='/' element={<Home/>} />
            <Route path='/apply-job/:id' element={<ApplyJob/>} />
-           <Route path='/application' element={<Application/>} />
+           <Route path='/applications' element={<Application/>} />
        </Routes>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,23 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
 import { AppContextProvider } from './context/AppContext.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <AppContextProvider>
-      <App />
-    </AppContextProvider>
+//Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+if(!PUBLISHABLE_KEY){
+  throw new Error("Missing Publishable Key")
+}
 
-  </BrowserRouter>,
+createRoot(document.getElementById('root')).render(
+
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+    <BrowserRouter>
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
+    </BrowserRouter>
+  </ClerkProvider>
+
 )
