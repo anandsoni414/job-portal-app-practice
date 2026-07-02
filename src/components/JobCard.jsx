cb9d1b3547bd26c89d05a3054ba42d992978d2c6
anandsoni414 <anand414soni@gmail.com>
2026-07-02 21:56:33 +0530

    Apply-job page setup

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const JobCard = ({job}) => {
+
+    const navigate = useNavigate()
+
   return (
     <div className='border border-gray-300 p-6 shadow rounded'>
         <div className='flex justify-between items-center'>
@@ -14,8 +18,8 @@ const JobCard = ({job}) => {
         </div>
         <p className='text-gray-500 text-sm mt-4' dangerouslySetInnerHTML={{__html:job.description.slice(0,150)}}></p>
         <div className='mt-4 flex gap-4 text-sm'>
-            <button className='bg-blue-600 text-white px-4 py-2 rounded'>Apply now</button>
-            <button className='border border-gray-600 text-gray-500 px-4 py-2 rounded'>Learn more</button>
+            <button onClick={()=> {navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className='bg-blue-600 text-white px-4 py-2 rounded'>Apply now</button>
+            <button onClick={()=> {navigate(`/apply-job/${job._id}`); scrollTo(0,0)}} className='border border-gray-600 text-gray-500 px-4 py-2 rounded'>Learn more</button>
         </div>
     </div>
   )
--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AppContext } from '../context/AppContext'
+import { useParams } from 'react-router-dom'
+import Loading from '../components/Loading'
+import Navbar from '../components/Navbar'
+import { assets } from '../assets/assets'
+import kconvert from 'k-convert'
 
 const ApplyJob = () => {
-  return (
-    <div>ApplyJob</div>
+
+  const {id} = useParams()
+
+  const [JobData, setJobData] = useState(null)
+
+  const {jobs} = useContext(AppContext)
+
+  const fetchJob = async () => {
+      const data = jobs.filter(job => job._id === id)
+      if(data.length != 0){
+         setJobData(data[0])
+      }
+  }
+  useEffect(() => {
+     if(jobs.length>0){
+        fetchJob()
+     }
+  },[id,jobs])
+
+  return JobData ? (
+    <>
+       <Navbar />
+       <div>
+          <div>
+             <div>
+               <div>
+                  <img src={JobData.companyId.image} alt="" />
+                  <div>
+                    <h1>{JobData.title}</h1>
+                    <div>
+                       <span>
+                          <img src={assets.suitcase_icon} alt="" />
+                          {JobData.companyId.name}
+                       </span>
+                       <span>
+                         <img src={assets.location_icon} alt="" />
+                         {JobData.location}
+                       </span>
+                       <span>
+                         <img src={assets.person_icon} alt="" />
+                         {JobData.level}
+                       </span>
+                       <span>
+                         <img src={assets.money_icon} alt="" />
+                         CTC: {kconvert.convertTo(JobData.salary)}
+                       </span>
+                    </div>
+                  </div>
+               </div>
+             </div>
+          </div>
+       </div>
+    </>
+  ) : (
+     <Loading />
   )
 }
 
